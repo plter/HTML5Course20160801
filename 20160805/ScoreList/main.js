ee7522fc1044d134b9f9b6a03ac31b0af6d97f78
梦宇 <xtiqin@163.com>
2016-08-05 10:46:45 +0800

    提交代码

---
+++ 20160805/ScoreList/main.js
@@ -0,0 +1,80 @@
+/**
+ * Created by plter on 8/5/16.
+ */
+
+(function () {
+
+    var container = document.getElementById("container");
+    var currentTable;
+
+    var scoreList = [
+        {name: "张三", score: 10},
+        {name: "李四", score: 12},
+        {name: "王五", score: 9},
+        {name: "赵六", score: 15},
+        {name: "铁蛋儿", score: 8}
+    ];
+
+    function createElement(tagName, content) {
+        var e = document.createElement(tagName);
+        if (content) {
+            e.innerHTML = content;
+        }
+        return e;
+    }
+
+    function scoreLinkClickedHandler() {
+        var tmp;
+        for (var i = 0; i < scoreList.length; i++) {
+            for (var j = i + 1; j < scoreList.length; j++) {
+                if (scoreList[i].score < scoreList[j].score) {
+                    tmp = scoreList[i];
+                    scoreList[i] = scoreList[j];
+                    scoreList[j] = tmp;
+                }
+            }
+        }
+
+        syncTable();
+    }
+
+    function syncTable() {
+        if (currentTable) {
+            container.removeChild(currentTable);
+        }
+
+        currentTable = generateTableByScoreList();
+        container.appendChild(currentTable);
+    }
+
+    function generateTableByScoreList() {
+        var table = createElement("table");
+
+        var tr = createElement("tr");
+        tr.appendChild(createElement("th", "名字"));
+        var th = createElement("th");
+        tr.appendChild(th);
+        var a = createElement("a", "分数");
+        a.href = "#";
+        a.onclick = scoreLinkClickedHandler;
+        th.appendChild(a);
+        table.appendChild(tr);
+
+        for (var i = 0; i < scoreList.length; i++) {
+            var student = scoreList[i];
+
+            tr = createElement("tr");
+            tr.appendChild(createElement("td", student.name));
+            tr.appendChild(createElement("td", student.score));
+            table.appendChild(tr);
+        }
+
+        return table;
+    }
+
+    function init() {
+        syncTable();
+    }
+
+    init();
+})();
