512c53a40d22ef58c08d807e62c8293cc2c07506
梦宇 <xtiqin@163.com>
2016-08-10 14:45:45 +0800

    hash页面跳转

--- 20160810/hashchange/main.js
+++ 20160810/hashchange/main.js
@@ -5,9 +5,34 @@
 
 (function () {
 
+    var container = document.querySelector("#container");
+
     window.onhashchange = function (event) {
-        console.log(event);
+        changePage();
+    };
+
+    function changePage() {
+        var hashObj = {};
+
+        var hashString = location.hash;
+        if (hashString && hashString != "") {
+            var kvStrs = location.hash.substr(1).split("&");
+            for (var i = 0; i < kvStrs.length; i++) {
+                var kv = kvStrs[i].split("=");
+                hashObj[kv[0]] = kv[1];
+            }
+        }
+
+        switch (hashObj.page) {
+            case "2":
+                container.innerHTML = "<img src='photo.jpg'>";
+                break;
+            default:
+                container.innerHTML = "<h1>Hello World</h1>";
+                break;
+        }
     }
 
+    changePage();
 
 })();
---
+++ 20160810/href/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by plter on 8/10/16.
+ */
+
+(function () {
+
+    // console.log(document.querySelector("#btngotoucai"));
+
+    document.querySelector("#btngotoucai").onclick = function () {
+        location.href = "http://ucai.cn";
+    };
+
+})();
