079b859a674987d00b8ee77dec72df3e713f421e
梦宇 <xtiqin@163.com>
2016-08-10 10:24:11 +0800

    上传作业示例

---
+++ 20160810/eyes/main.js
@@ -0,0 +1,32 @@
+/**
+ * Created by plter on 8/10/16.
+ */
+
+(function () {
+
+    var eyeballPositionY = 88;
+    var leftEyeballGlobalPositionX = 57;
+    var rightEyeballGlobalPositionX = 157;
+    var R = 15;
+    var eyeBallStartX = 20;
+    var eyeBallStartY = 20;
+    var leftEye = document.querySelector(".left .eyeball");
+    var rightEye = document.querySelector(".right .eyeball");
+
+
+    function setEyeballPosition(eye, dx, dy) {
+        var angle = Math.atan(dy / dx);
+        var direction = dx > 0 ? 1 : -1;
+        var eyeX = eyeBallStartX + R * Math.cos(angle) * direction;
+        var eyeY = eyeBallStartY + R * Math.sin(angle) * direction;
+        eye.style.left = eyeX + "px";
+        eye.style.top = eyeY + "px";
+    }
+
+
+    document.addEventListener("mousemove", function (event) {
+        setEyeballPosition(leftEye, event.pageX - leftEyeballGlobalPositionX, event.pageY - eyeballPositionY);
+        setEyeballPosition(rightEye, event.pageX - rightEyeballGlobalPositionX, event.pageY - eyeballPositionY);
+    });
+
+})();
