159c0b17dd0374727932a08462615a14f2d0059c
梦宇 <xtiqin@163.com>
2016-08-15 11:04:45 +0800

    update

--- 20160815/images/main.js
+++ 20160815/images/main.js
@@ -8,13 +8,33 @@
 
     var content = document.querySelector("#content");
     var contentX = 0;
+    var imgWidth = 200;
+    var duration = 20;
 
+    var index = 0;
 
     setInterval(function () {
-        contentX--;
 
-        // content.style.left = contentX + "px";
-        content.style.transform = "translateX(" + contentX + "px)";
-    }, 100);
+        index++;
+        if (index > 2) {
+            index = 0;
+        }
+
+        var endX = -imgWidth * index;
+        var distanceMoved = 0;
+        var endDistance = Math.abs(endX - contentX);
+        var speed = (endX - contentX) / duration;
+        var absSpeed = Math.abs(speed);
+        var id = setInterval(function () {
+            contentX += speed;
+            distanceMoved += absSpeed;
+            if (distanceMoved > endDistance) {
+                contentX = endX;
+                clearInterval(id);
+            }
+
+            content.style.left = contentX + "px";
+        }, 20);
+    }, 3000);
 
 })();
