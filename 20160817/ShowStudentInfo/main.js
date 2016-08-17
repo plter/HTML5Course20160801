1fa0d357694c111ff2c551c5b23c7d74f3fad143
梦宇 <xtiqin@163.com>
2016-08-17 15:06:35 +0800

    添加移除功能

--- 20160817/ShowStudentInfo/main.js
+++ 20160817/ShowStudentInfo/main.js
@@ -4,32 +4,51 @@
 
 (function () {
 
-    function createStudent(name, age, gender) {
+    function CreateStudent(name, age, gender) {
 
         var self = {name: name, age: age, gender: gender};
 
-        self.createHTMLElement = function () {
-            var div = document.createElement("div");
+        self.getNode = function () {
+            return self.htmlNode;
+        };
+
+        function createHtmlNode() {
+            self.htmlNode = document.createElement("div");
             var a = document.createElement("a");
             a.innerHTML = self.name;
             a.href = "#";
             a.onclick = function () {
                 alert("名字:" + self.name + "\n年龄:" + self.age + "\n性别:" + self.gender);
             };
-            div.appendChild(a);
-            return div;
-        };
+            self.htmlNode.appendChild(a);
+
+            var closeBtn = document.createElement("button");
+            closeBtn.innerHTML = "Close";
+            self.htmlNode.appendChild(closeBtn);
+            closeBtn.onclick = function () {
+                self.htmlNode.parentNode.removeChild(self.htmlNode);
+            }
+        }
+
+        function init() {
+            createHtmlNode();
+        }
+
+        init();
 
         return self;
     }
 
     var students = [
-        createStudent("张三", 10, "男"),
-        createStudent("李四", 12, "男"),
-        createStudent("丽丽", 9, "女")
+        CreateStudent("张三", 10, "男"),
+        CreateStudent("李四", 12, "男"),
+        CreateStudent("丽丽", 9, "女")
     ];
 
+    console.log(students);
+
     for (var i = 0; i < students.length; i++) {
-        document.body.appendChild(students[i].createHTMLElement());
+        var student = students[i];
+        document.body.appendChild(student.getNode());
     }
 })();
