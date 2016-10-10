eea8f2e8f25a74c34f96f8b1c89c1ea8da2c207a
梦宇 <xtiqin@163.com>
2016-10-10 14:33:10 +0800

    绘制遮罩

---
+++ 20161010/Mask/main.js
@@ -0,0 +1,50 @@
+/**
+ * Created by plter on 10/10/16.
+ */
+
+(function () {
+
+
+    var img;
+    var context = document.getElementById("canvas").getContext("2d");
+    var circleX = 0, circleY = 100, speed = 1;
+
+    function loadImage() {
+        img = new Image();
+        img.src = "image.jpg";
+    }
+
+    function render() {
+
+        circleX += speed;
+        if (circleX > 300) {
+            speed = -1;
+        }
+        if (circleX < 100) {
+            speed = 1;
+        }
+
+        context.clearRect(0, 0, 400, 300);
+
+        context.save();
+        context.beginPath();
+        context.arc(circleX, circleY, 100, 0, Math.PI * 2);
+        context.closePath();
+        context.clip();
+
+        context.drawImage(img, 0, 0);
+
+        context.restore();
+
+        requestAnimationFrame(render);
+    }
+
+    function init() {
+        loadImage();
+        render();
+    }
+
+    init();
+
+
+})();
