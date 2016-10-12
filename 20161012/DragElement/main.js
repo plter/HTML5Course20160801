327cb2c023a2487ba0cd9c083bae7992c4a029d6
梦宇 <xtiqin@163.com>
2016-10-12 15:13:24 +0800

    query

--- 20161012/DragElement/main.js
+++ 20161012/DragElement/main.js
@@ -5,22 +5,39 @@
 
 (function () {
 
-    var text = document.querySelector("#text");
-    var targetContainer = document.querySelector("#target-container");
+    // var text = document.querySelector("#text");
+    // var targetContainer = document.querySelector("#target-container");
 
     function addListeners() {
-        text.addEventListener("dragstart", function (e) {
-            e.dataTransfer.setData("text/plain", text.id);
-        });
+        // text.addEventListener("dragstart", function (e) {
+        //     e.dataTransfer.setData("text/plain", text.id);
+        // });
+        //
+        // targetContainer.addEventListener("dragover", function (e) {
+        //     e.preventDefault();
+        // });
+        // targetContainer.addEventListener("drop", function (e) {
+        //     e.preventDefault();
+        //     e.stopPropagation();
+        //     // console.log(e.dataTransfer.getData("text/plain"));
+        //
+        //     var element = document.getElementById(e.dataTransfer.getData("text/plain"));
+        //     if (element.parentNode) {
+        //         element.parentNode.removeChild(element);
+        //     }
+        //     this.appendChild(element);
+        // });
 
-        targetContainer.addEventListener("dragover", function (e) {
-            e.preventDefault();
+        $("#text").on("dragstart", function (e) {
+            e.originalEvent.dataTransfer.setData("text/plain", this.id);
         });
-        targetContainer.addEventListener("drop", function (e) {
+        $(".container").on("dragover", function (e) {
             e.preventDefault();
-            // console.log(e.dataTransfer.getData("text/plain"));
+        }).on("drop", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
 
-            var element = document.getElementById(e.dataTransfer.getData("text/plain"));
+            var element = document.getElementById(e.originalEvent.dataTransfer.getData("text/plain"));
             if (element.parentNode) {
                 element.parentNode.removeChild(element);
             }
