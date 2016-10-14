67109249cbdb99978cfaa741492afc61e8609e7a
梦宇 <xtiqin@163.com>
2016-10-14 10:15:17 +0800

    实现作业要求

---
+++ 20161014/DrawRect/routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get("/draw", function (req, res) {
+
+    let x = req.query.x,
+        y = req.query.y,
+        width = req.query.width,
+        height = req.query.height,
+        color = req.query.color;
+
+    res.send(`
+<html>
+<body>
+<canvas id="canvas" width="400" height="300"></canvas>
+<script>
+var x = ${x};
+var y = ${y};
+var width = ${width};
+var height = ${height};
+var color = "${color}";
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+context.fillStyle = color;
+context.fillRect(x,y,width,height);
+
+</script>
+</body>
+</html>
+`);
+});
+module.exports = router;
