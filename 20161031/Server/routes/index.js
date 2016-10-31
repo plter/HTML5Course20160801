67e4e5f7118ac84d17288ed7a80ab8baf523636b
梦宇 <xtiqin@163.com>
2016-10-31 14:18:02 +0800

    动态页面

--- 20161031/Server/routes/index.js
+++ 20161031/Server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
     res.render('index', {title1: 'Express', data: "Hello"});
 });
 
+router.get("/hello", function (req, res) {
+    res.send("Hello");
+});
+
 module.exports = router;
--- 20161031/Server/routes/users.js
+++ 20161031/Server/routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get("/hello", function (req, res) {
+    res.send("User Hello");
+});
+
+router.get("/a/b/c/d.html", function (req, res) {
+    res.send("Hello Java");
 });
 
 module.exports = router;
