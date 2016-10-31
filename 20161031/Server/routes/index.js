c11b0c39d3312d0cd0a5638d70c8a13ccbf02c32
梦宇 <xtiqin@163.com>
2016-10-31 14:52:47 +0800

    jsonp

--- 20161031/Server/routes/index.js
+++ 20161031/Server/routes/index.js
@@ -10,4 +10,14 @@ router.get("/hello", function (req, res) {
     res.send("Hello");
 });
 
+router.get("/data", function (req, res) {
+    console.log(req.headers.referer);
+    if (req.headers.referer.indexOf("localhost:63341") != -1) {
+        res.jsonp({name: "ucai", age: 10});
+    } else {
+        res.send("恶意攻击别人网站可耻");
+    }
+
+});
+
 module.exports = router;
