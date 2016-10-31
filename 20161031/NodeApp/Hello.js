c180536f6332676378f2bb7b8e237a6833109bfe
梦宇 <xtiqin@163.com>
2016-10-31 10:02:36 +0800

    Nodejs模块

---
+++ 20161031/NodeApp/Hello.js
@@ -0,0 +1,12 @@
+/**
+ * Created by plter on 2016/10/31.
+ */
+
+class Hello {
+
+    sayHello() {
+        console.log("Hello World");
+    }
+}
+
+module.exports = Hello;
---
+++ 20161031/NodeApp/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by plter on 2016/10/31.
+ */
+
+var Hello = require("./Hello");
+
+var h = new Hello();
+h.sayHello();
