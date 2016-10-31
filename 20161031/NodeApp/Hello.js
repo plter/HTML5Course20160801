c42bf64db684e0062b6beb0d1edf8697bd25877f
梦宇 <xtiqin@163.com>
2016-10-31 10:46:01 +0800

    解构与Nodejs模块

--- 20161031/NodeApp/Hello.js
+++ 20161031/NodeApp/Hello.js
@@ -2,6 +2,8 @@
  * Created by plter on 2016/10/31.
  */
 
+"use strict";
+
 class Hello {
 
     sayHello() {
@@ -9,4 +11,34 @@ class Hello {
     }
 }
 
-module.exports = Hello;
+
+class Student {
+
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+    sayHello() {
+        console.log("Hello " + this.name);
+    }
+
+    get name() {
+        return this._name;
+    }
+
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        this._age = value;
+    }
+}
+
+module.exports = {Hello: Hello, Student: Student};
--- 20161031/NodeApp/app.js
+++ 20161031/NodeApp/app.js
@@ -2,7 +2,28 @@
  * Created by plter on 2016/10/31.
  */
 
-var Hello = require("./Hello");
+// const Hello = require("./Hello");
+
+const {Hello, Student} = require("./Hello");
 
 var h = new Hello();
 h.sayHello();
+
+
+var s = new Student("ucai", 10);
+s.sayHello();
+
+// var arr = [1, undefined, 3];
+// var [a,b = 5,c] = arr;
+// console.log(a, b, c);
+
+// function getNumbers() {
+//     return [1, 2, 3];
+// }
+//
+// var [a,b,c] = getNumbers();
+// console.log(a, b, c);
+
+// var obj = {name: "ucai", age: 10};
+// var {name, age}=obj;
+// console.log(name, age);
