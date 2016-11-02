dc539559588227e4e8569fc9f26fdff4db0e8c03
梦宇 <xtiqin@163.com>
2016-11-02 14:24:42 +0800

    上传 JSON格式 的数据

--- 20161102/ExpressServer/public/xhr/main.js
+++ 20161102/ExpressServer/public/xhr/main.js
@@ -17,18 +17,27 @@
         xhr.onload = function () {
             resultDiv.innerHTML = xhr.responseText;
         };
+
+        //GET方式
         // xhr.open("GET", "/hello?user=" + this['user'].value);
         // xhr.send();
 
+        //POST URL参数
         // xhr.open("POST", "/hello");
         // xhr.setRequestHeader("Content-type", 'application/x-www-form-urlencoded; charset=UTF-8');
         // xhr.send("user=" + this['user'].value);
 
-        xhr.open("POST", "/upload");
-        var fd = new FormData();
-        fd.append("user", this['user'].value);
-        fd.append("age", this['age'].value);
-        xhr.send(fd);
+        //POST FormData
+        // xhr.open("POST", "/upload");
+        // var fd = new FormData();
+        // fd.append("user", this['user'].value);
+        // fd.append("age", this['age'].value);
+        // var fd = new FormData(this);
+        // xhr.send(fd);
+
+        //POST JSON
+        xhr.open("POST", "/hello.json");
+        xhr.send(JSON.stringify({user: this['user'].value, age: this['age'].value}));
     });
 
 })();
--- 20161102/ExpressServer/routes/index.js
+++ 20161102/ExpressServer/routes/index.js
@@ -17,8 +17,19 @@ router.post("/hello", function (req, res) {
     res.send(`Hello ${req.body.user}`);
 });
 
-router.post("/upload", upload.none(), function (req, res) {
+router.post("/upload", upload.none()/*只接受上传文本数据*/, function (req, res) {
     res.send(`Hello ${req.body.user},and your age is ${req.body.age}`);
+});
+
+router.post("/hello.json", (req, res)=> {
+    let allData = Buffer.alloc(0);
+    req.on("data", data=> {
+        allData = Buffer.concat([allData, data]);
+    });
+    req.on("end", ()=> {
+        var obj = JSON.parse(allData.toString());
+        res.json({message: `Your name is ${obj.user},your age is ${obj.age}`});
+    });
 });
 
 module.exports = router;
