486efdbc133fb1844a27084f1489a29e4e5faeaf
梦宇 <xtiqin@163.com>
2016-11-02 16:56:22 +0800

    上传文件进度提示

--- 20161102/ExpressServer/public/UploadImage/aysncupload.js
+++ 20161102/ExpressServer/public/UploadImage/aysncupload.js
@@ -6,15 +6,25 @@
 
     var form = document.querySelector("form");
     var resultDiv = document.querySelector("#result");
+    var progressBar = $("#progress-bar");
 
     form.addEventListener("submit", function (e) {
         e.preventDefault();
 
         var xhr = new XMLHttpRequest();
+        xhr.upload.onprogress = function (e) {
+            // console.log(e.loaded / e.total * 100 + "%");
+
+            progressBar.progressbar({value: e.loaded, max: e.total});
+        };
+
+        // xhr.onprogress = function (e) {};//用于侦听下载进度
+
         xhr.addEventListener("load", function () {
             console.log(xhr.responseText);
 
-            resultDiv.innerHTML = "<img src='" + xhr.responseText + "'>";
+            resultDiv.innerHTML = "<video autoplay='autoplay' controls='controls' width='400' height='300' src='" + xhr.responseText + "'></video>";
+            // resultDiv.innerHTML = "<img src='" + xhr.responseText + "'>";
         });
 
         xhr.open("POST", "/apis/upload");
