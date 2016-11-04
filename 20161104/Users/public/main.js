3fb19e189fa15dbeab0b877dd7573c3515c1af20
梦宇 <xtiqin@163.com>
2016-11-04 14:41:59 +0800

    登陆实现

--- 20161104/Users/public/main.js
+++ 20161104/Users/public/main.js
@@ -5,6 +5,7 @@
 (function () {
 
     var registerAlertDiv = $("#register").find(".alert-div");
+    var loginAlertDiv = $("#login").find(".alert-div");
 
     $("#form-register").submit(function (e) {
         e.preventDefault();
@@ -40,5 +41,30 @@
         });
     });
 
+    $("#form-login").submit(function (e) {
+        e.preventDefault();
+
+        loginAlertDiv.html("正在连接服务器...");
+        $.post("/apis/login", {
+            user: this['user'].value,
+            pass: this['pass'].value
+        }).done(function (data) {
+            console.log(data);
+            switch (data.state) {
+                case 1:
+                    loginAlertDiv.html("登陆成功");
+                    break;
+                case 5:
+                case 6:
+                    loginAlertDiv.html("用户名或密码错误");
+                    break;
+                default:
+                    loginAlertDiv.html("登陆失败");
+                    break;
+            }
+        }).fail(function () {
+            loginAlertDiv.html("无法连接服务器");
+        });
+    });
 
 })();
--- 20161104/Users/routes/apis.js
+++ 20161104/Users/routes/apis.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var createConn = require("../sources/CreateConn");
 
+
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+var r = router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-router.post('/register', function (req, res) {
-
+function checkUserAndPassword(req, res, next) {
     if (!req.body.user) {
         res.json({state: 3, message: "No user name present"});//
         return;
@@ -18,17 +18,48 @@ router.post('/register', function (req, res) {
         return;
     }
 
-    let conn = createConn();
-    conn.connect1().then(result=> {
-        return conn.query1("INSERT INTO `users` (`user`,`pass`) VALUES (?,?)", [req.body.user, req.body.pass]);
-    }).then(result=> {
-        res.json({state: 1, message: "OK"});
+    next();
+}
 
-        conn.end();
-    }).catch(error=> {
-        console.log(error);
-        // res.json({state: 2});//注册失败
-        res.json({state: error.errno, message: error.code});
+router.post("/register", checkUserAndPassword)
+    .post('/register', function (req, res) {
+        let conn = createConn();
+        conn.connect1().then(result=> {
+            return conn.query1("INSERT INTO `users` (`user`,`pass`) VALUES (?,?)", [req.body.user, req.body.pass]);
+        }).then(result=> {
+            res.json({state: 1, message: "OK"});
+
+            conn.end();
+        }).catch(error=> {
+            console.log(error);
+            // res.json({state: 2});//注册失败
+            res.json({state: error.errno, message: error.code});
+        });
     });
-});
+
+
+router.post("/login", checkUserAndPassword)
+    .post("/login", function (req, res) {
+        let conn = createConn();
+        conn.connect1().then(function () {
+            return conn.query1("SELECT * FROM `users` WHERE `user`=?", [req.body.user]);
+        }).then(function (rows) {
+            if (rows.length) {
+                var result = rows[0];
+                if (req.body.pass == result.pass) {
+                    res.json({state: 1, message: "OK"});
+                } else {
+                    res.json({state: 6, message: "Password wrong"});
+                }
+            } else {
+                res.json({state: 5, message: "No such user"});//用户名不存在
+            }
+
+            conn.end();
+        }).catch(function (error) {
+            console.log(error);
+            res.json({state: error.errno, message: error.code});
+        });
+    });
+
 module.exports = router;
