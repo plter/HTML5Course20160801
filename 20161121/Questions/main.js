f14d4c9cb80cdc9d4d72672f134e7fe22ffb775c
梦宇 <xtiqin@163.com>
2016-11-21 14:42:15 +0800

    完成作业示例

--- 20161121/Questions/main.js
+++ 20161121/Questions/main.js
@@ -5,37 +5,50 @@
 angular.module("questions", []).controller("main", function ($scope, $http) {
 
 
-    var questionIndex = 0, question, questions;
+    var question, questions, score = 0;
+    $scope.questionIndex = 0;
+    $scope.atLastQuestion = false;
     $scope.title = "正在加载...";
+    var questionForm = document.querySelector("#form-question");
 
     $scope.nextQuestionHandler = function () {
         showNextQuestion();
     };
 
+    $scope.btnSubmitBtnClicked = function () {
+        alert("最终得分" + (score + (isCurrentAnswerCorrect() ? 1 : 0)));
+    };
+
+    function isCurrentAnswerCorrect() {
+        return question.correct == questionForm["answer"].value;
+    }
+
     function showNextQuestion() {
 
-        var form = document.querySelector("#form-question");
-        var selectedAnswer = form["answer"].value;
-        console.log(selectedAnswer == question.correct);
+        if ($scope.questionIndex < questions.length - 1) {
+            if (isCurrentAnswerCorrect()) {
+                score += 1;
+            }
 
-        // questionIndex++;
-        // if (questionIndex < questions.length) {
-        //     showQuestion();
-        // } else {
-        //     alert("已经到最后一题");
-        // }
+            $scope.questionIndex++;
+            showQuestion();
+        } else {
+            alert("已经到最后一题");
+        }
     }
 
     function showFirstQuestion() {
-        questionIndex = 0;
+        $scope.questionIndex = 0;
+        score = 0;
         showQuestion();
     }
 
     function showQuestion() {
-        question = questions[questionIndex];
+        question = questions[$scope.questionIndex];
         var first = question;
         $scope.title = first.title;
         $scope.answers = first.answers;
+        $scope.atLastQuestion = $scope.questionIndex == questions.length - 1;
     }
 
     $http.get("data.json").then(function (result) {
